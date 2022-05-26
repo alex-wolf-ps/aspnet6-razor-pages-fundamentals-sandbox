7991e74299a7cd0863ef7d865aa0b4744f48cf2f
Alex Wolf <alexwolf@mcirosoft.com>
2022-05-26 12:26:37 -0400

    09

--- 01_After/09/WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
+++ 01_After/09/WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
@@ -14,15 +14,17 @@ namespace WiredBrainCoffeeAdmin.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly IHttpClientFactory factory;
+        private readonly IWebHostEnvironment _env;
 
         public IDictionary<string, string> OrderStats { get; set; }
 
         public List<SurveyItem> SurveyResults { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpFactory)
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpFactory, IWebHostEnvironment env)
         {
             _logger = logger;
             this.factory = httpFactory;
+            _env = env;
         }
 
         public async Task<IActionResult> OnGet()
@@ -35,7 +37,8 @@ namespace WiredBrainCoffeeAdmin.Pages
             OrderStats = JsonSerializer.Deserialize<IDictionary<string, string>>(responseData);
 
             var rawJson = System.IO.File
-                .ReadAllText("wwwroot/sampledata/survey.json");
+               .ReadAllText(Path.Combine(_env.ContentRootPath,
+                   "wwwroot/sampledata/survey.json"));
 
             SurveyResults = JsonSerializer.Deserialize<List<SurveyItem>>(rawJson);
 
