ab432610dd96d4ff886192bdd0b6a283df78b949
Alex Wolf <alexwolf@mcirosoft.com>
2022-05-26 11:57:14 -0400

    07

--- 00_Before/07/WiredBrainCoffeeAdmin/Migrations/WiredContextModelSnapshot.cs
+++ 00_Before/07/WiredBrainCoffeeAdmin/Migrations/WiredContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace WiredBrainCoffeeAdmin.Migrations
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "6.0.3");
+            modelBuilder.HasAnnotation("ProductVersion", "6.0.4");
 
             modelBuilder.Entity("WiredBrainCoffeeAdmin.Data.Product", b =>
                 {
